323a7485e80914de20004e0bbae277effb2f522c
edra2000 <glume@live.com>
2025-04-30 16:43:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchBinanceData() {
   try {
-    const res = await fetch('/api/binance');
+    const res = await fetch('https://api.binance.com/api/v3/ticker/24hr');
     const data = await res.json();
 
     const container = document.getElementById('cards-container');
